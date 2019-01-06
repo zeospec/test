dc313df118bfec69de42b2baed5419f9671ccae9
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2019-01-06 06:29:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev && \
 cd ./_site && \
-remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
+remote_repo=${"https://${GH_TOKEN}@${GH_REF}":-`git config remote.origin.url`} && \
 remote_branch="gh-pages" && \
 git init && \
 git config user.name "zeospec" && \
 git config user.email "www.aruntejathefriend@gmail.com" && \
 git add . && \
-git commit -m'build' && \
+git commit -m'Deployed to Github Pages' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
