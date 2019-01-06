e9b8724d7ee6a4791a4bb68032a56c2aa55e4547
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2019-01-06 06:33:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev && \
 cd ./_site && \
-remote_repo=${"https://${GH_TOKEN}@${GH_REF}":-`git config remote.origin.url`} && \
+remote_repo=${https://${GH_TOKEN}@${GH_REF}:-`git config remote.origin.url`} && \
 remote_branch="gh-pages" && \
 git init && \
 git config user.name "zeospec" && \
