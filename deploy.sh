224ef1841357011e0f2399205e25e324c7c42cef
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2019-01-08 22:26:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ set -ev && \
 cd ./_site && \
 remote_branch="gh-pages" && \
 git init && \
-git config user.name "zeospec" && \
-git config user.email "www.aruntejathefriend@gmail.com" && \
+git config user.name "Travis-CI" && \
+git config user.email "contact@travis-ci.com" && \
 git add . && \
 git commit -m'Deployed to Github' && \
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:$remote_branch > /dev/null 2>&1 && \
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
